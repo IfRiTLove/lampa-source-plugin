ccc45221e11f6737eec3aacd696a785df5174961
IfRiTLove <159574273+IfRiTLove@users.noreply.github.com>
2026-06-06 16:11:17 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,11 +1,65 @@
 (function () {
     'use strict';
 
-    alert('LAMPA SOURCE LOADED');
+    alert('Lampa Source Debug');
 
-    console.log('LAMPA SOURCE LOADED');
+    function log(msg) {
+        console.log('[LS]', msg);
 
-    if (window.Lampa) {
-        Lampa.Noty.show('LAMPA SOURCE LOADED');
+        try {
+            Lampa.Noty.show(msg);
+        } catch(e){}
     }
+
+    [
+        'app',
+        'full',
+        'activity',
+        'player',
+        'torrent',
+        'search'
+    ].forEach(function(eventName){
+
+        try {
+            Lampa.Listener.follow(eventName, function(e){
+                log(eventName + ': ' + (e.type || 'unknown'));
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+
+})();(function () {
+    'use strict';
+
+    alert('Lampa Source Debug');
+
+    function log(msg) {
+        console.log('[LS]', msg);
+
+        try {
+            Lampa.Noty.show(msg);
+        } catch(e){}
+    }
+
+    [
+        'app',
+        'full',
+        'activity',
+        'player',
+        'torrent',
+        'search'
+    ].forEach(function(eventName){
+
+        try {
+            Lampa.Listener.follow(eventName, function(e){
+                log(eventName + ': ' + (e.type || 'unknown'));
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+
 })();
