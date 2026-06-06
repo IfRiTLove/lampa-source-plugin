89e821259a6ea67634c9c409645e8fe62d38eb74
IfRiTLove <159574273+IfRiTLove@users.noreply.github.com>
2026-06-06 16:07:47 +0300

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,60 +1,11 @@
 (function () {
     'use strict';
 
-    const API_URL = 'https://130-162-220-139.sslip.io';
+    alert('LAMPA SOURCE LOADED');
 
-    function log(msg) {
-        console.log('[Lampa Source]', msg);
-        Lampa.Noty.show('LS: ' + msg);
-    }
+    console.log('LAMPA SOURCE LOADED');
 
-    function startPlugin() {
-        log('plugin loaded');
-
-        Lampa.Listener.follow('full', function (e) {
-            log('full event: ' + e.type);
-
-            if (e.type !== 'complite') return;
-
-            setTimeout(function () {
-                try {
-                    const activity = e.object && e.object.activity;
-                    if (!activity) {
-                        log('no activity');
-                        return;
-                    }
-
-                    const render = activity.render();
-                    if (!render || !render.length) {
-                        log('no render');
-                        return;
-                    }
-
-                    log('render ok');
-
-                    const torrent = render.find('.view--torrent');
-                    log('torrent buttons: ' + torrent.length);
-
-                    const movie = e.data && e.data.movie;
-                    if (movie) {
-                        log('movie: ' + (movie.title || movie.name || 'no title'));
-                    } else {
-                        log('no movie data');
-                    }
-
-                } catch (err) {
-                    console.error('[Lampa Source diagnostic error]', err);
-                    log('error: ' + err.message);
-                }
-            }, 800);
-        });
-    }
-
-    if (window.appready) {
-        startPlugin();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') startPlugin();
-        });
+    if (window.Lampa) {
+        Lampa.Noty.show('LAMPA SOURCE LOADED');
     }
 })();
